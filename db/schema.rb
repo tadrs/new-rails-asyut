a9f7174ffcbe88169adf24be541f66780abeb9c3
tadrs <tadrosgamal992@gmail.com>
2026-06-14 01:20:59 +0300

    make migration to add address column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_221334) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_13_221857) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_13_221334) do
 
   create_table "users", force: :cascade do |t|
     t.string "DOB"
+    t.string "address", default: "", null: false
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
