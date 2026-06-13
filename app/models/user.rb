1a289c8f0142f37ead00a0c1d8f6bd1087f68854
tadrs <tadrosgamal992@gmail.com>
2026-06-14 01:14:39 +0300

    add valifation to user model and manke new index with new migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :DOB, presence: true
+    validates :phone_number, presence: true
+    validates :email, presence: true,uniqueness: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
