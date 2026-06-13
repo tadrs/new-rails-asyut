c1fdbc4f2422780db2d16e67d568447331939229
tadrs <tadrosgamal992@gmail.com>
2026-06-14 01:08:30 +0300

    add user model and make migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_083257) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_13_220725) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "DOB"
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "phone_number"
     t.datetime "updated_at", null: false
   end
 end
