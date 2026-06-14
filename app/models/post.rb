e9b3dcbb08230a4aa969cb68f5746bf01e660f02
tadrs <tadrosgamal992@gmail.com>
2026-06-14 23:40:39 +0300

    add editor model and post_editor model and make relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :creator, class_name: "User", foreign_key: :creator_id, optional: true
+  has_many :post_editors
+  has_many :editors, through: :post_editors
 end
